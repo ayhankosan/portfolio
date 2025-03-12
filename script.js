fc584a56313cbfcf1a3adc4562cbf3208c63f657
ayhankosan <61837043+ayhankosan@users.noreply.github.com>
2025-03-12 10:39:46 -0700

    jekyll
    jekyll

--- script.js
+++
@@ -1,5 +0,0 @@
-// Toggle mobile menu visibility
-document.getElementById('menu-toggle').addEventListener('click', function () {
-  const navList = document.querySelector('nav ul');
-  navList.classList.toggle('active');
-});
